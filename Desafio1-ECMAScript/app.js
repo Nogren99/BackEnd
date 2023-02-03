0229e6077a5ae6c7c0d5cc1d2f17ac6c684f1c8d
Nogren99 <nogren23@gmail.com>
2023-02-02 21:45:55 -0300

    code fix

--- Desafio1-ECMAScript/app.js
+++ Desafio1-ECMAScript/app.js
@@ -9,22 +9,29 @@ class ProductManager {
     }
 
     addProduct = (title, description, price, thumbnail, code, stock) => {
-        const product = {
-            title: title,
-            description: description,
-            price: price,
-            thumbnail: thumbnail,
-            code:code,
-            stock:stock
-        }
-        
-        if (this.products.length === 0) {
-            product.id = 1;
-        } else {
-            product.id = this.products[this.products.length - 1].id + 1;
+
+        if(this.products.some(e=>e.code==code)){
+            console.log("Producto repetido")
+        }else{
+            const product = {
+                title: title,
+                description: description,
+                price: price,
+                thumbnail: thumbnail,
+                code:code,
+                stock:stock
+            }
+            
+            if (this.products.length === 0) {
+                product.id = 1;
+            } else {
+                product.id = this.products[this.products.length - 1].id + 1;
+            }
+    
+            this.products.push(product);
         }
 
-        this.products.push(product);
+        
     }
 
     getProductById = (id) => {
@@ -57,3 +64,6 @@ console.log("--------")
 console.log("Busco producto inexistente")
 productTest.getProductById(3)
 console.log("--------")
+
+console.log("Intento agregar producto codigo repetido")
+productTest.addProduct("Campera","de cuero",10000,null,456,10)
